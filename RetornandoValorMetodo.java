0c4f2afe21d202cce7d52021dbb035efffc6d2be
James <jamessilva.perpart@gmail.com>
2025-01-03 14:39:33 -0300

    chamando o metodo passando os valores de boleto e pix

--- RetornandoValorMetodo.java
+++ RetornandoValorMetodo.java
@@ -1,12 +1,12 @@
 public class RetornandoValorMetodo {
     public static void main(String[] metodosdepagamento){
         // Cria um metodo de pagamento pix
-        MetodoPagamentoInterface pagamentoPix = new PixMetodo();
+        MetodoPagamentoInterface pagamentoPix = new PixMetodo(22.50);
         double valorPagamento = pagamentoPix.valorCobranca();
         String metodoPagamento = pagamentoPix.metodoCobranca();
 
         // Cria um pagamento por boleto:
-        MetodoPagamentoInterface pagamentoBoleto = new BoletoMetodo();
+        MetodoPagamentoInterface pagamentoBoleto = new BoletoMetodo(30.64);
         double valorPagamento2 = pagamentoBoleto.valorCobranca();
         String metodoPagamento2 = pagamentoBoleto.metodoCobranca();
 
