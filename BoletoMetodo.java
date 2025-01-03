d596b5ecb8ee0152cccf4891fb023fd0055693c5
James <jamessilva.perpart@gmail.com>
2025-01-03 14:32:36 -0300

    Adicionado o construtor

--- BoletoMetodo.java
+++ BoletoMetodo.java
@@ -2,6 +2,11 @@ public class BoletoMetodo implements MetodoPagamentoInterface {
     double valorPagamento = 10.22;
     String metodoPagamento = "Boleto";
 
+    public BoletoMetodo(double valorPagamento, String metodoPagamento){
+        this.valorPagamento = valorPagamento;
+        this.metodoPagamento = metodoPagamento;
+    }
+
     @Override
     public double valorCobranca(){
         return valorPagamento;
