5934771f5158a6cb9fabda2d5feaaa3a059d1b91
James <jamessilva.perpart@gmail.com>
2025-01-03 14:34:08 -0300

    construtor adicionado

--- PixMetodo.java
+++ PixMetodo.java
@@ -2,6 +2,11 @@ public class PixMetodo implements MetodoPagamentoInterface{
     double valorPagamento = 32.50;
     String metodoPagamento = "Pix";
 
+    public PixMetodo(double valorPagamento, String metodoPagamento){
+        this.valorPagamento = valorPagamento;
+        this.metodoPagamento = metodoPagamento;
+    }
+
     @Override
     public double valorCobranca(){
         return valorPagamento;
